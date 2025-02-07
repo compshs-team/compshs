84501b92e6e587b7c545d2bd2a48f74d7206e607
simondelarue <sim.delarue@gmail.com>
2025-02-07 12:42:11 +0100

    Updated config

--- setup.py
+++ setup.py
@@ -3,19 +3,19 @@
 
 """The setup script."""
 
-from setuptools import setup, find_packages
+# from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
+# with open('README.rst') as readme_file:
+#    readme = readme_file.read()
 
-requirements = []
+# requirements = []
 
-test_requirements = ['pytest']
+# test_requirements = ['pytest']
 
-setup(
-    packages=find_packages(),
-    install_requires=requirements,
-    tests_require=test_requirements,
-    test_suite='tests',
-    zip_safe=False,
-)
+#setup(
+#    packages=find_packages(),
+#    install_requires=requirements,
+#    tests_require=test_requirements,
+#    test_suite='tests',
+#    zip_safe=False,
+#)
