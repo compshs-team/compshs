d3884d40506b08d969a935e0308c92d4da63f4c8
simondelarue <sim.delarue@gmail.com>
2025-02-07 15:35:11 +0100

    Coverage test

--- compshs/utils/tests/test_check.py
+++ compshs/utils/tests/test_check.py
@@ -11,3 +11,5 @@ class TestChecks(unittest.TestCase):
     def test_check_is_zero(self):
         with self.assertRaises(TypeError):
             is_zero('toto')
+        assert is_zero(0) is True
+        assert is_zero(-1) is False
