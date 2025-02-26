168fd2e8f6dde865fc4105fcea78c27cc7541e78
simondelarue <sim.delarue@gmail.com>
2025-02-26 11:53:34 +0100

    updated docstring

--- compshs/utils/check.py
+++ compshs/utils/check.py
@@ -13,16 +13,27 @@ def is_zero(value: int) -> bool:
 
 
 def load_lang(lang: str = 'en_core_web_sm'):
+    """Load (trained) Spacy pipeline.
+
+    Parameters
+    ----------
+    lang: str
+        Spacy pipeline name (default is the english pipeline `en_core_web_sm`).
+
+    Returns
+    -------
+    Trained spacy pipeline, otherwise blank minimal pipeline.
+    """
     try:
         return spacy.load(lang)
     except OSError:
-        print(f'Error: Could not load spacy model {lang}.')
-        print(f'Downloading the model...')
+        print(f'Error: Could not load spacy pipeline {lang}.')
+        print(f'Downloading the Spacy pipeline...')
         spacy.cli.download(lang)
 
         try:
             return spacy.load(lang)
         except Exception as e:
-            print(f'Failed to load the model: {e}.')
-            print(f'Fall back to minimal blank model, i.e. tokenizer only.')
+            print(f'Failed to load the Spacy pipeline: {e}.')
+            print(f'Fall back to minimal blank pipeline, i.e. tokenizer only.')
             return spacy.blank(lang)
