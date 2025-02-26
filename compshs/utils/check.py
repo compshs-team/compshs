a341dfa1a7d81f53317f86e1afcb52d35e7eda4f
simondelarue <sim.delarue@gmail.com>
2025-02-26 11:41:03 +0100

    added function to load spacy model

--- compshs/utils/check.py
+++ compshs/utils/check.py
@@ -1,10 +1,8 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Created in 2025
 @author: Simon Delarue <simon.delarue@telecom-paris.fr>
 """
-import warnings
+import spacy
 
 
 def is_zero(value: int) -> bool:
@@ -12,3 +10,19 @@ def is_zero(value: int) -> bool:
         raise TypeError(f'Expected an integer but got {type(value).__name__}.')
     else:
         return value == 0
+
+
+def load_lang(lang: str = 'en_core_web_sm'):
+    try:
+        return spacy.load(lang)
+    except OSError:
+        print(f'Error: Could not load spacy model {lang}.')
+        print(f'Downloading the model...')
+        spacy.cli.download(lang)
+
+        try:
+            spacy.load(lang)
+        except Exception as e:
+            print(f'Failed to load the model: {e}.')
+            print(f'Fall back to minimal blank model, i.e. tokenizer only.')
+            return spacy.blank(lang)
